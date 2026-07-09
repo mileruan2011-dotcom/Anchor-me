1cb78086dd8cf8210d7353c087e398d4066c0726
mileruan2011-dotcom <mile.ruan.2011@gmail.com>
2026-07-09 22:09:32 +0200

    Fix Safari blank dashboard and simplify anchor cockpit

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'determination-anchor-v1';
+const CACHE = 'determination-anchor-v3';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
